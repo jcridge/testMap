b3b13b36e6d58701c41f6f3a8154f0f32176edc4
jcridge <crij1_12@uni.worc.ac.uk>
2015-02-06 12:15:11 +0000

    Lots

--- js/main.js
+++ js/main.js
@@ -1,14 +1,56 @@
-$(document).ready(function () {
+$(document).on("pageshow", "#page2", function(event){
+    console.log("Page Show");
     changeSize();
+    initialize();
+    google.maps.event.trigger(map, 'resize');
 });
+
+//$(document).ready(function () {
+    //changeSize();
+//});
 
 function changeSize() {
     var bodyheight = $(window).height();
+    console.log(bodyheight);
     $("#map-canvas").height(bodyheight - 60);
     $(window).resize(function () {
         var bodyheight = $(window).height();
         $("#map-canvas").height(bodyheight - 60);
     });
 }
+
+
+	  function initialize() {
+          console.log("Drawing Map");
+          var mapOptions = {
+            center: { lat: -34.397, lng: 150.644},
+            zoom: 8,
+            disableDefaultUI: true
+          };
+          var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+      
+          // Try HTML5 geolocation
+          if(navigator.geolocation) {
+              navigator.geolocation.getCurrentPosition(function(position) {
+                  var pos = new google.maps.LatLng(position.coords.latitude,
+                                                   position.coords.longitude);
+                  var marker = new google.maps.Marker({
+                      position: pos,
+                      map: map,
+                  });
+                  
+                  map.setCenter(pos);
+              }, 
+            function() {
+                  handleNoGeolocation(true);
+              });
+          } 
+          else {
+        // Browser doesn't support Geolocation
+        handleNoGeolocation(false);
+      }
+          
+      }
+	  
 
 
